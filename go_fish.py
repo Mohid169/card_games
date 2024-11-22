466ba0285d94fcd470e018f5a71c8f6b7550fd34
Mohid Khan <mohidkhan169@gmail.com>
2024-11-21 21:03:08 -0500

    psuhing code

--- go_fish.py
+++ go_fish.py
@@ -1,7 +1,7 @@
 import random
 
 
-class Card:
+class Card():
     VALID_RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     VALID_SUITS = ["spade", "ace", "hearts", "clubs"]
 
@@ -18,7 +18,7 @@ class Card:
         return f"{self.rank}{self.suit}"
 
 
-class DeckofCards:
+class DeckofCards():
     def __init__(self):
         self.cards = []
 
@@ -33,3 +33,31 @@ class DeckofCards:
         if not self.cards:
             raise ValueError("Cannot draw from an empty deck")
         return self.cards.pop()
+
+class Player():
+    def __init__(self, name, hand = None):
+        self.name = name
+        self.hand = hand if hand is not None else []
+        self.books = []
+    
+    def give_cards(self, rank):
+        cards_to_give = []
+        remaining_hand = []
+        for card in self.hand:
+            if rank == card.rank:
+                cards_to_give.append(card)
+
+            else:
+                remaining_hand.append(card)
+                
+
+        self.hand = remaining_hand
+
+        return cards_to_give
+
+    def check_books(self):
+       pass 
+
+
+        
+
