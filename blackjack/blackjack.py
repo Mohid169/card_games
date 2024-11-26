7bd513e31f4ca08e9caefa1ac79beb08701328b4
Mohid Khan <mohidkhan169@gmail.com>
2024-11-26 14:30:00 -0500

    modifications

---
+++ blackjack/blackjack.py
@@ -0,0 +1,57 @@
+import random 
+
+class Card:
+    VALID_RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10","ace", "jack", "queen", "king"]
+    VALID_SUITS =["clubs", "spade", "ace", "hearts"]
+
+    def __init__(self, rank, suit):
+        if rank not in self.VALID_RANKS:
+            raise("invalid rank")
+        
+        if rank not in self.VALID_SUITs:
+            raise("invalid suit")
+
+        self.rank = rank
+        self.suit = suit
+
+    def name(self):
+        return f"{self.rank, self.suit}"
+    
+
+class DeckOfCards():
+    def __init__(self):
+        self.cards = []
+
+        for rank in Card.VALID_RANKS:
+            for suit in Card.VALID_SUITS:
+                self.cards.append(Card(rank, suit))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+    
+    def draw(self, num_cards = 1):
+        drawn_cards = []
+        for _ in range(num_cards):
+            if not self.cards:
+                print("deck is empty")
+                return []
+            
+            drawn_cards.append(self.cards.pop())
+
+        return drawn_cards
+
+class Player():
+    def __init__(self, name, hand = None):
+        self.name = name
+        self.hand = []
+    pass 
+
+class Game():
+    pass 
+
+
+
+
+    
+
+    
